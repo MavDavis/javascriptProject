2e1cfb86c6d7ea8b47aff1c93f49b5509fd5581b
MavDavis <ekwesimarvelous@gmail.com>
2022-06-19 21:07:40 +0100

    commit

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ let result = document.querySelector(".results");
 let url = "https://jsonplaceholder.typicode.com/posts";
 let titles = document.querySelector("input");
 let bodys = document.querySelector("textarea");
+let navLink = document.querySelector(".navLink");
 
 function navbar() {
     let navList = document.getElementsByClassName("nav-links");
     for (var i = 0; i < navList.length; i++) {
         navList[i].addEventListener("click", function(e) {
-            e.preventDefault();
-            let navLink = (document.querySelector(".navLink").style.display = "none");
-            let signButton = (document.querySelector(".sign").style.display = "none");
+            navLink.style.display = "none";
+
             let nav = document.querySelector(".nav");
             nav.classList.toggle("show-div");
             console.log(e);
@@ -24,18 +24,10 @@ hamburger.addEventListener("click", function(e) {
     let nav = document.querySelector(".nav");
     nav.classList.toggle("show-div");
 
-    let navLink = document.querySelector(".navLink");
-    let signButton = document.querySelector(".sign");
-
-    if (
-        navLink.style.display === "block" &&
-        signButton.style.display === "block"
-    ) {
+    if (navLink.style.display === "block") {
         navLink.style.display = "none";
-        signButton.style.display = "none";
     } else {
         navLink.style.display = " block";
-        signButton.style.display = "block";
     }
     navbar();
 });
